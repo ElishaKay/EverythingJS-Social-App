3683ad9a200b18dc493562fcd32da42cdc97fc7f
Elisha Kramer <Kramer1346@gmail.com>
2017-02-15 11:20:46 +0200

    'hey

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ mongoose.connect(process.env.MONGOLAB_GREEN_URI ||'mongodb://localhost/beers');
 
 
 
-
 var Beer = require("./models/BeerModel");
 var Review= require('./models/ReviewModel');
 
@@ -60,94 +59,13 @@ var app = express();
 // for fb authenticate
 app.use(expressSession({ secret: 'mySecretKey' }));
 
-app.use(passport.initialize());
-app.use(passport.session());
 
-
-app.use(bodyParser.json());   // This is the type of body we're interested in
-app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 app.use(express.static('views'));
 
-
-// Change the callbackURL you see below
-// every time that youre developing locally
-// vs deploying to heroku domain
-
-passport.use(new FacebookStrategy({
-    clientID: '1886152074941466',
-    clientSecret: '1d9904d87a3e1b3a0b46692cadcc26ef',
-    
-    // For development
-    // callbackURL: "http://localhost:8000/auth/facebook/callback",
-    
-    // For Deployment
-    callbackURL: "https://letsamp.herokuapp.com/auth/facebook/callback",
-    profileFields: ['id', 'displayName', 'photos', 'email']
-  },
-
-    function(accessToken, refreshToken, profile, done) {
-    console.log("accessToken:");
-    console.log(accessToken);
-
-    console.log("refreshToken:");
-    console.log(refreshToken);
-
-    console.log("profile:");
-    console.log(profile);
-
-    return done(null, profile);
-  }
-));
-
-// used to serialize the user for the session
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-// used to deserialize the user
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-
-
-// To see just the JSON that facebook returns back, 
-// simply uncomment this line
-
-
-  // route for showing the profile page
-  app.get('/profile', function(req, res) {
-    console.log(req.user);
-    res.render('profile.ejs', {
-      user: req.user // get the user out of session and pass to template
-    });
-  });
-
-
-app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', {
-    successRedirect : '/profile',
-    failureRedirect : '/facebookCanceled'
-  }));
-
-app.get('/profile', function(req, res) {
-  res.json(req.user);
-});
-
-app.get('/facebookCanceled', function(req, res) {
-  res.send("fail!");
-});
-
-
-// app.use('/userPage', facebookAuthenticate(req, res, next));
-
-
-// app.get('/userPage', fucntion(req, res){
-//   res.send('userPage.html')
-// })
 
 app.get('/', function (req, res) {
   res.send("You are inside the fullstack project")
@@ -220,14 +138,6 @@ app.post('/beers/:id/reviews/', function(req, res, next) {
     });  
   });
 });
-
-
-// fb auth
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/auth/facebook', passport.authenticate('facebook'));
 
 
 
